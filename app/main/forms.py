79481fb776fa65a6bbc995059a09fd2aac8a95b6
amani-joseph <mr.amanijoseph@gmail.com>
2022-03-06 20:42:24 +0300

    Added The HTML templates pages to routes to

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField, RadioField
+from wtforms import StringField, TextAreaField, SubmitField, RadioField
 from wtforms.validators import Required
 
-
-class CommentsForm(FlaskForm):
-    comment = TextAreaField('Comment', validators=[Required()])
-    vote = RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
-    submit = SubmitField('SUBMIT')
-
-
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.', validators=[Required()])
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title', validators=[Required()])
+    author = StringField('Author', validators=[Required()])
+    pitch_content = TextAreaField('Write Pitch', validators=[Required()])  
+    category = RadioField('Pick Category', choices=[('Pickup Lines', 'Pickup Lines'), ('Interview Pitch', 'Interview Pitch'), ('Product Pitch', 'Product Pitch'), ('Promotion Pitch', 'Promotion Pitch')], validators=[Required()])  
     submit = SubmitField('Submit')
 
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Say something about yourself...', validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CommentsForm(FlaskForm):
+    body = TextAreaField('Write a comment...', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,8 @@ from flask import render_template, request, redirect, url_for, abort
 from . import main
 from .forms import CommentsForm, UpdateProfile, PitchForm
 from ..models import Comment, Pitch, User
-# from flask_login import login_required, current_user
-# from .. import db, photos
+from flask_login import login_required, current_user
+from .. import db, photos
 
 # import markdown2
 
@@ -23,7 +23,7 @@ def index():
 
 # this section consist of the category root functions
 
-@main.route('/inteview/pitches/')
+@main.route('/interview/pitches/')
 def interview():
     '''
     View root page function that returns the index page and its data
