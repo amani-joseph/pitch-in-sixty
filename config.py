ec95e411c411afc6ae07aca22cd3b4ee0fa5f0fb
amani-joseph <mr.amanijoseph@gmail.com>
2022-03-09 11:53:31 +0300

    Production to deployment 2

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class ProdConfig(Config):
 
 class TestConfig(Config):
 #    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://j03:j03@localhost/pitch'
-   SQLALCHEMY_DATABASE_URI = 'postgres://cwybneiomrugfl:cd378ab9f71cd27cb49e8b0a23c949c5cbacf57b84e7af7c7108fe33bfbf841c@ec2-52-70-205-234.compute-1.amazonaws.com:5432/d3fpocs4p9ko5v'
+   SQLALCHEMY_DATABASE_URI = 'postgresql://cwybneiomrugfl:cd378ab9f71cd27cb49e8b0a23c949c5cbacf57b84e7af7c7108fe33bfbf841c@ec2-52-70-205-234.compute-1.amazonaws.com:5432/d3fpocs4p9ko5v'
     
 
 class DevConfig(Config):
