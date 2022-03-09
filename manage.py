9cd8faaac6f8a817929f676a5b5b70edaa999e72
amani-joseph <mr.amanijoseph@gmail.com>
2022-03-09 22:17:02 +0300

    Deployment attempt 10

--- manage.py
+++ manage.py
@@ -14,15 +14,18 @@ manager.add_command('server', Server)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity = 2).run(tests)
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User)
+    return dict(app=app, db=db, User=User)
+
 
 if __name__ == '__main__':
     manager.run()
